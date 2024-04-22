1a1f84a9e3f23615c28f89f98e6214d310e13fcd
dev-chpark <xpmxf5@gmail.com>
2024-04-22 13:57:17 +0900

    ♻️  refactor: BubbleSort refactor

--- src/main/java/org/example/sorting/BubbleSort.java
+++ src/main/java/org/example/sorting/BubbleSort.java
@@ -6,24 +6,25 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int[] array = {13, 51, 34, 32, 22, 67, 4, 31, 90};
-
+        System.out.println("(before)array = " + Arrays.toString(array));
         bubbleSort(array);
-
-        System.out.println("array = " + Arrays.toString(array));
+        System.out.println("(after)array = " + Arrays.toString(array));
     }
 
-    private static void bubbleSort(int[] array) {
+    public static void bubbleSort(int[] array) {
         int n = array.length;
-
+        boolean swapped;
         for (int i = 0; i < n - 1; i++) {
+            swapped = false;
             for (int j = 0; j < n - i - 1; j++) {
-
                 if (array[j] > array[j + 1]) {
-                    int tmp = array[j];
+                    int temp = array[j];
                     array[j] = array[j + 1];
-                    array[i + 1] = tmp;
+                    array[j + 1] = temp;
+                    swapped = true;
                 }
             }
+            if (!swapped) break;
         }
     }
 }
