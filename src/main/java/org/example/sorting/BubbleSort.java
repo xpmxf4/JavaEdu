cc0a168795e117bef72f49198705c25e5f397157
dev-chpark <xpmxf5@gmail.com>
2024-04-22 13:58:06 +0900

    ⚙️  chore: BubbleSort 특징 주석 추가

--- src/main/java/org/example/sorting/BubbleSort.java
+++ src/main/java/org/example/sorting/BubbleSort.java
@@ -17,6 +17,7 @@ public class BubbleSort {
         for (int i = 0; i < n - 1; i++) {
             swapped = false;
             for (int j = 0; j < n - i - 1; j++) {
+                // 특징 : 인접한 두 data를 비교하며, 큰 값을 배열 끝으로 이동
                 if (array[j] > array[j + 1]) {
                     int temp = array[j];
                     array[j] = array[j + 1];
